88404a474a9679f934634a293bfd6dcd287e0937
CO4768472-fallo <co4768472@gmail.com>
2026-03-17 21:39:07 -0600

    ejercicio de python 1

---
+++ ejercicio-1.py
@@ -0,0 +1,7 @@
+nombre_completo = input("Por favor, ingresa tu nombre completo: ")
+
+print("Tu nombre en mayúsculas es:", nombre_completo.upper())
+print("Tu nombre en minúsculas es:", nombre_completo.lower())
+
+cantidad_caracteres = len(nombre_completo)
+print("La cantidad de caracteres de tu nombre es:", cantidad_caracteres)
