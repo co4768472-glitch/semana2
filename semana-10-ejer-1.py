7a47ddc7981462d46f3fe1f9e0a018a651189a82
CO4768472-fallo <co4768472@gmail.com>
2026-03-26 15:12:25 -0600

    Laboratorio: ejercicio 1 completado

---
+++ semana-10-ejer-1.py
@@ -0,0 +1,26 @@
+def transformar_texto(texto, opcion):
+
+    if opcion == 1:
+
+        # 1. Convertir a mayúsculas
+
+        resultado = texto.upper()
+
+    elif opcion == 2:
+
+        resultado = texto.lower()
+
+    elif opcion == 3:
+
+        resultado = texto.capitalize()
+
+    else:
+
+        resultado = "Opción no válida"
+
+    return resultado
+
+
+prueba = transformar_texto("hola ingeniero", 3)
+
+print(prueba)
