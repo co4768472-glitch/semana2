32ba1eb8bd8fb113fbaaa1d670891bfaf786f518
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:29:48 -0600

    Ejer 3: Prefijos y mayusculas

---
+++ lab-ejer-3.py
@@ -0,0 +1,9 @@
+texto_original = "ING. Geovany"
+print(f"texto original: {texto_original}")
+
+sin_prefijo = texto_original.removeprefix("ING. ")
+print(f"texto sin prefijo: {sin_prefijo}")
+
+texto_mayus = sin_prefijo.upper()
+print(f"texto en mayusculas: {texto_mayus}")
+
