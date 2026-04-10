48a1c4fb1cddebfe7320afbc50d1f72eece365ed
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:29:16 -0600

    Ejer 2: Formato title y replace

---
+++ Lab-ejer-2.py
@@ -0,0 +1,11 @@
+texto_originar = "su nombre"
+print(f"texto original: {texto_originar}")
+
+texto_titulo = texto_originar.title()
+print(f"el texto con titulo es: {texto_titulo}")
+
+texto_final = texto_titulo.replace("Su Nombre", "Su Apellido")
+print(f"el texto final es: {texto_final}")
+
+
+
