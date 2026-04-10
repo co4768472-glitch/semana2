c1e0bfa6e5d403be716976802d04f446dfec2bbd
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:32:36 -0600

    Ejer 8: Comillas triples y splitlines

---
+++ lab-ejer-8.py
@@ -0,0 +1,14 @@
+poema = """ing digame como puedo hacer para que ella me ame
+, y para no pensarla tanto
+y si me responde le debo una tasa de cafe JAJAJAJA"""
+print(poema)
+
+print("-" * 20)
+
+cantidad_a = poema.count("a")
+lista_oraciones = poema.splitlines()
+
+print("la letra a aparece",cantidad_a,"veces")
+print("el texto en lista es", lista_oraciones)
+
+
