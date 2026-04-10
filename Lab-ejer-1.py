c6a1ff026bc4109f8e96c640fac4688b29a3ecce
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:28:29 -0600

    Ejer 1: Limpieza strip y conteo

---
+++ Lab-ejer-1.py
@@ -0,0 +1,8 @@
+texto_animal = "     elefante    "
+texto_limpio = texto_animal.strip()
+texto_contar = texto_limpio.count("e")
+
+print("El texto limpio es:",texto_limpio)
+print("El texto tiene", texto_contar,"letras e")
+
+
