617708796240d5d31c7a7c01d7451080c6b316ee
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:34:21 -0600

    Ejer 11: Strip, title y center

---
+++ lab-ejer-11.py
@@ -0,0 +1,13 @@
+texto = " el nido matinal "
+
+espacios = texto.strip()
+mayuscula = espacios.title()
+
+centrar = mayuscula.center(30,"-")
+print("texto original:", texto)
+print(f"texto completo: {centrar}")
+
+
+
+
+
