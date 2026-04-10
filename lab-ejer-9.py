f39ff1d0018c7ee87665ce119d58ee6c7422823c
CO4768472-fallo <co4768472@gmail.com>
2026-04-09 22:33:11 -0600

    Ejer 9: Reemplazo y upper

---
+++ lab-ejer-9.py
@@ -0,0 +1,12 @@
+texto = "Any time y Anytime"
+
+reemplazar = texto.replace("Any time", "Always") 
+
+mayusculas = reemplazar.upper()
+
+print(f"texto original: {texto}")
+print(f"texto reemplazado: {reemplazar}")
+print(f"el texto en mayusculas es: {mayusculas}")
+
+
+
