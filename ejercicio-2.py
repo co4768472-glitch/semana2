75a9d880c0c0e2bed4753b5d6cafb8918fe1e01e
CO4768472-fallo <co4768472@gmail.com>
2026-04-19 12:23:53 -0600

    Ejer 2: Clasificacion de categoria por edad

---
+++ ejercicio-2.py
@@ -0,0 +1,8 @@
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 60:
+    print("Eres un adulto mayor")
+elif edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
