c80996c0efd69b18440dc4ee1c35385709effd78
CO4768472-fallo <co4768472@gmail.com>
2026-04-19 12:22:06 -0600

    Ejer 1: Validacion de numero positivo, negativo o cero

---
+++ ejercicio-1.py
@@ -0,0 +1,9 @@
+numero_positivo = int(input("Ingrese un numero: "))
+
+if numero_positivo > 0:
+    print("El numero es positivo")
+elif numero_positivo == 0:
+    print("El numero es cero")
+else:
+    print("El numero es negativo")
+    
